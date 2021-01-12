7ba1399631cceb0c6db0316f09ae5d4e4712c80d
Nail Tosun <nailtosun07@gmail.com>
2021-01-12 15:41:59 +0300

    Update startup.m

--- MATLAB/startup.m
+++ MATLAB/startup.m
@@ -1,1 +1,9 @@
-
+%% if there is no startup.m file, create one
+edit(fullfile(userpath,'startup.m'))
+%% Then put the code
+set(0,'defaulttextfontsize',18);
+set(0,'defaultaxesfontsize',18);
+set(0,'defaulttextfontname','Times');
+set(0,'defaulttextinterpreter','latex');  
+set(0, 'defaultAxesTickLabelInterpreter','latex');  
+set(0, 'defaultLegendInterpreter','latex');
